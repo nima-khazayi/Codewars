03e83f1ed98c54da50afc72eae71ba52aa0dd902
Nima <Nimakhazayi7@gmail.com>
2025-03-18 01:17:44 +0330

    Update Digital_Roots.py

--- Python/Digital Root/Digital_Roots.py
+++ Python/Digital Root/Digital_Roots.py
@@ -7,35 +7,19 @@ def digital_root(n):
     while (num1 > 0):   
         num1 = num1 // 10
         digits += 1
-          
+
     num1 = n
-    check = n // 10
     # if it has only 1 digit return the number
-    if (check > 0):
+    if (digits != 1):
         # otherwise we calculate sum of the number digits
         while (num1 > 0):
             for i in range(digits):
                 sum += num1 % 10
                 num1 = num1 // 10
-            # set digits zero to count it for the result number    
-            digits = 0
-            
-            # saving the sum that if in the below while we noticed that 
-            # result has one digit we should return that
-            sum1 = sum
-            
-            while (sum > 0):
-                sum = sum // 10
-                digits += 1
-                
-            # otherwise calculated summation is being set as the new number
-            num1 = sum1
-            
-            if (digits == 1):
-                break
+        # call function to recalculate digital roots
+        # passing the argument of sum as the summation 
+        # of the last number digits
+        return digital_root(sum)    
 
-        return num1
-            
     else:
         return n
-        
