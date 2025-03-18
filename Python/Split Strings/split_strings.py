42f66886b30fe8c6b0ff6035c633860f2c511094
Nima <Nimakhazayi7@gmail.com>
2025-03-18 16:02:30 +0330

    Update split_strings.py

--- Python/Split Strings/split_strings.py
+++ Python/Split Strings/split_strings.py
@@ -3,22 +3,11 @@ def solution(s):
     string = []
     
     # condition for even number of characters in string    
-    if (len(s) % 2 == 0):
+    if (len(s) % 2 != 0):  
+        s = s + "_"
+        
+    for i in range(0, len(s), 2):
+        str = s[i] + s[i+1] # add chars to each other 2 by 2
+        string.append(str) # add them to the final list
     
-        for i in range(0, len(s), 2):
-            str = s[i] + s[i+1] # add chars to each other 2 by 2
-            string.append(str) # add them to the final list
-    
-        return string
-        
-    # condition for odd number of characters in string
-    else:
-        
-        for i in range(0, len(s) - 1, 2):
-            str = s[i] + s[i+1] # add chars to each other 2 by 2
-            string.append(str) # add them to the final list
-            
-        str = s[-1] + "_" # for the last char, add "_" 
-        string.append(str) # add it to the final list
-        
-        return string
+    return string
